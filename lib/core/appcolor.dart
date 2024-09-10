c3be418a09d3e992bc0337e62af06dae6cb5ace2
Muhammed Anas AK <anazbinashraf2@gmail.com>
2024-09-10 11:44:35 +0530

    initial commit

---
+++ lib/core/appcolor.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static final Color primaryColor = HexColor('#15995e');
+  static final Color secondaryColor = HexColor('#394757');
+  static final Color success = HexColor('#15995e');
+  static final Color warning = HexColor('#d09e12');
+  static final Color danger = HexColor('#de3e3e');
+  static final Color accentColor = HexColor('#e74c3c');
+  static final Color backgroundColor = HexColor('#ecf0f1');
+  static final Color textColor = HexColor('#2c3e50');
+  static final Color buttonColor = HexColor('#15995e');
+  static final Color appBar = HexColor("#DEF7F0");
+  static final Color menubackgroundColor = HexColor("#F0F3F5");
+   static final Color cancelButton = HexColor("#FF1E1E");
+    static final Color submitButton = HexColor("#23AD00");
+
+}
